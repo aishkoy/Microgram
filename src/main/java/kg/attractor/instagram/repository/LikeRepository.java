91c749d4578e425b5ce1e43bfcae9d4c62cffccd
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-21 00:56:29 +0600

    feat: implemented basic repositories

---
+++ src/main/java/kg/attractor/instagram/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package kg.attractor.instagram.repository;
+
+import kg.attractor.instagram.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
