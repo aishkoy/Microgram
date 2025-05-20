a866d60a50700fcf4a5b33f76c0930214fb2ead5
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-20 23:25:19 +0600

    Initial commit

---
+++ src/test/java/kg/attractor/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
