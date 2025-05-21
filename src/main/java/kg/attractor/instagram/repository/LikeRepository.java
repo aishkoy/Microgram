0e132a38c035da8ccff388c44f0b6cdc91a45e33
I.Maksat <maksat@MacBookAir.lan>
2025-05-21 23:18:13 +0600

    feat: added Repository

--- src/main/java/kg/attractor/instagram/repository/LikeRepository.java
+++ src/main/java/kg/attractor/instagram/repository/LikeRepository.java
@@ -1,9 +1,14 @@
 package kg.attractor.instagram.repository;
 
 import kg.attractor.instagram.entity.Like;
+import kg.attractor.instagram.entity.LikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, LikeId> {
+    List<Like> findByIdPostId(Long postId);
+    Long countByIdPostId(Long postId);
 }
