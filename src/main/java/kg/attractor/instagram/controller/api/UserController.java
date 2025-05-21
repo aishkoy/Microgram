a53683dfc4e65a61fec045ef2e272b7d722d8bb6
I.Maksat <maksat@MacBookAir.lan>
2025-05-21 18:33:55 +0600

    feat: wrote an api to get an avatar

---
+++ src/main/java/kg/attractor/instagram/controller/api/UserController.java
@@ -0,0 +1,22 @@
+package kg.attractor.instagram.controller.api;
+
+import jakarta.validation.Valid;
+import kg.attractor.instagram.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{userId}/avatar")
+    public ResponseEntity<?> getUserAvatar(@PathVariable @Valid Long userId) {
+            return userService.getAvatarByUserId(userId);
+    }
+}
